84d7fc9eb64b2f84a348ef9d70b6811351157fe0
hassan-devDz <hassan26102017@gmail.com>
2020-09-29 16:37:17 +0100

    📝 add ScrollSpy

--- js/main.js
+++ js/main.js
@@ -5,3 +5,12 @@ var carousel = new bootstrap.Carousel(myCarousel, {
     touch: true,
     pause: false
 })
+
+var scrollSpy = new bootstrap.ScrollSpy(document.body, {
+    target: '#navbarNav',
+    offset: 50
+})
+
+var scrollSpyContentEl = document.getElementById('navbarNav')
+var scrollSpy = bootstrap.ScrollSpy.getInstance(scrollSpyContentEl)
+
