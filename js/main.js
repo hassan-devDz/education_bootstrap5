c223a71935d8abcd549a348bf5de54d10a66c8d7
hassan-devDz <hassan26102017@gmail.com>
2020-09-29 11:35:59 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+
+
+var myCarousel = document.querySelector('#carousel-kafaa')
+var carousel = new bootstrap.Carousel(myCarousel, {
+    interval: 1000000,
+    wrap: true,
+    touch: true,
+    pause: false
+})
+
+
